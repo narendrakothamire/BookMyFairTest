4718bd9a696c98d7b7be82f5564881531477c3f9
techguy <techguynarendra@gmail.com>
2016-06-05 22:09:40 +0530

    Initial commit

---
+++ app/src/main/java/com/softwares/swamisamarth/bookmyfairtest/AdapterFairDetailsViewPager.java
@@ -0,0 +1,25 @@
+package com.softwares.swamisamarth.bookmyfairtest;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Narendra on 6/5/2016.
+ */
+public class AdapterFairDetailsViewPager extends FragmentStatePagerAdapter {
+
+    public AdapterFairDetailsViewPager(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/softwares/swamisamarth/bookmyfairtest/FragmentFairDetails.java
@@ -0,0 +1,30 @@
+package com.softwares.swamisamarth.bookmyfairtest;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentFairDetails extends Fragment {
+
+
+    public FragmentFairDetails() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fair_details, container, false);
+
+        return view;
+    }
+
+}
